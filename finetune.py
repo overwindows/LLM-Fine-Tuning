cb34ad38ad9957b0079e6cae03d669f48ccbc3bb
overwindows <overwindows@gmail.com>
2023-05-25 11:35:17 +0000

    modified:   .gitignore 	new file:   ds_config.json 	modified:   finetune.ipynb 	modified:   finetune.py 	modified:   finetune.sh 	modified:   requirements.txt 	modified:   tokenizer.ipynb 	modified:   utils.py 	modified:   z3_ds_config_v0.json

--- finetune.py
+++ finetune.py
@@ -1,8 +1,8 @@
 import torch
 import transformers
-from transformers import BloomTokenizerFast, BloomForCausalLM, TrainingArguments
+from transformers import BloomTokenizerFast, BloomForCausalLM, TrainingArguments, DataCollatorWithPadding
 from datasets import load_dataset
-from utils import ModifiedTrainer, tokenise_data, data_collator
+from utils import ModifiedTrainer, data_collator, tokenize_data, data_collator_ex
 from utils import ModelArguments, DataArguments
 
 
@@ -11,30 +11,33 @@ def main():
         (ModelArguments, DataArguments, TrainingArguments)
     )
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-
+    # assert torch.cuda.is_available()
+    # device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    
     model_name = model_args.model_name_or_path
-    # print(model_name)
-    tokeniser = BloomTokenizerFast.from_pretrained(
+    tokenizer = BloomTokenizerFast.from_pretrained(
         f"{model_name}", add_prefix_space=True
     )
-    model = BloomForCausalLM.from_pretrained(f"{model_name}").to(device)
-
+    model = BloomForCausalLM.from_pretrained(f"{model_name}", use_cache=False).cuda()
+        
     data_name = data_args.data_name_or_path
-    dataset = load_dataset(data_name)
-    input_ids = tokenise_data(dataset, tokeniser)
-
+    dataset=load_dataset(data_name, cache_dir='/cpfs01/user/Wuchen2023/datasets', streaming=True, split='train')
+    dataset = dataset.with_format('torch')
+    def preprocess_function(example):
+        return tokenizer(example['content'], truncation=True, max_length=1024, padding="max_length")
+    # tokenized_dataset = tokenize_data(dataset, tokenizer)
+    tokenized_dataset = dataset.map(preprocess_function, batched=True)
     model.gradient_checkpointing_enable() 
     model.is_parallelizable = True
     model.model_parallel = True
-
-    # train
+    
+    # print(next(iter(tokenized_dataset)))
+    
     trainer = ModifiedTrainer(
         model=model,
-        train_dataset=input_ids,
+        train_dataset=tokenized_dataset,
         args=training_args,
-        data_collator=data_collator,
+        data_collator=data_collator_ex,
     )
     trainer.train()
 
--- finetune.sh
+++ finetune.sh
@@ -1,10 +1,14 @@
-deepspeed --num_gpus=8 finetune.py \
-    --model_name_or_path bigscience/bloom-7b1  \
-    --per_device_train_batch_size 2 \
-    --num_train_epochs 8 \
-    --logging_steps 10 --fp16 \
-    --deepspeed z3_ds_config.json \
-    --output_dir output --overwrite_output_dir
+# deepspeed --num_gpus=8 finetune.py \
+#     --model_name_or_path /cpfs01/user/Wuchen2023/models/bloom-7b1  \
+#     --data_name_or_path tatsu-lab/alpaca \
+#     --per_device_train_batch_size 32 \
+#     --learning_rate 1e-05 --weight_decay 0.01 --warmup_steps 300 \
+#     --lr_scheduler_type='cosine' \
+#     --gradient_checkpointing \
+#     --num_train_epochs 3 \
+#     --logging_steps 10 --fp16 \
+#     --deepspeed ds_config.json \
+#     --output_dir output --overwrite_output_dir
 
 # deepspeed --num_gpus=4 finetune-alpaca.py \
 #     --model_name_or_path bigscience/bloom-1b7 \
@@ -24,12 +28,15 @@ deepspeed --num_gpus=8 finetune.py \
 #     --logging_steps 10 \
 #     --output_dir output
 
-# python finetune-alpaca.py \
-#     --model_name_or_path bigscience/bloom-7b1 \
-#     --per_device_train_batch_size 2 \
-#     --gradient_accumulation_steps 1 \
-#     --num_train_epochs 2 \
-#     --learning_rate 2e-5 \
-#     --fp16 True \
-#     --logging_steps 10 \
-#     --output_dir output --overwrite_output_dir
+deepspeed --num_gpus=8 finetune.py \
+    --model_name_or_path /cpfs01/user/Wuchen2023/models/bloom-7b1 \
+    --data_name_or_path codeparrot/codeparrot-clean-train \
+    --per_device_train_batch_size 16 --gradient_accumulation_steps 4 --max_steps 500000 \
+    --learning_rate 1e-05 --weight_decay 0.01 --warmup_steps 300 \
+    --lr_scheduler_type='cosine' \
+    --gradient_checkpointing \
+    --num_train_epochs 3 \
+    --logging_steps 10 --fp16 \
+    --deepspeed ds_config.json \
+    --output_dir output 
+    # --overwrite_output_dir
