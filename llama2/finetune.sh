461776f482b74de387513aacb78fbfb13e2a3872
snova-chenw <overwindows@live.com>
2024-02-06 22:28:46 +0000

    Update finetune.sh to improve model training parameters

--- llama2/finetune.sh
+++ llama2/finetune.sh
@@ -4,6 +4,7 @@ LOG_STEPS=10
 SAVE_INTERVAL=1
 LEARNING_RATE=1e-6
 NUM_TRAIN_EPOCHS=8
+GRAD_ACC_STEPS=2
 
 DEEPSPEED_CONF=../ds_configs/z3_ds_config.json
 DEEPSPEED_PORT=9902
@@ -17,7 +18,6 @@ DATA_PATH=/import/snvm-sc-scratch2/fengluh/web_master/training_data/train/bookin
 CACHE_DIR=/import/snvm-sc-podscratch3/chenw/dataset_cache
 
 MODEL_PATH=/import/ml-sc-nlpcheckpoints-scratch/jonathanl/generic_checkpoints/llama_2/Llama-2-7b-hf
-
 OUTPUT_DIR=/import/snvm-sc-podscratch3/chenw/model/llama2_7b_ft_gpu
 
 # deepspeed --num_gpus=4 --master_port 9901 finetune.py \
@@ -34,8 +34,9 @@ deepspeed --num_gpus=$DEEPSPEED_GPUS --master_port $DEEPSPEED_PORT finetune.py \
     --model_name_or_path $MODEL_PATH \
     --data_name_or_path $DATA_PATH \
     --data_cache_dir $CACHE_DIR \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 4 \
     --learning_rate $LEARNING_RATE \
+    --gradient_accumulation_steps $GRAD_ACC_STEPS \
     --num_train_epochs $NUM_TRAIN_EPOCHS --max_steps 1000 \
     --logging_steps $LOG_STEPS --fp16 \
     --save_steps 16 \
