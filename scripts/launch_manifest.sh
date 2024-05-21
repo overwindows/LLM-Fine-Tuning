a5e953eb5aa505332f5ad790861ca0b06d9e4518
Chen Wu <overwindows@gmail.com>
2024-05-22 03:22:12 +0800

    update gpu manifest

--- scripts/launch_manifest.sh
+++ scripts/launch_manifest.sh
@@ -1,8 +1,14 @@
-MODEL_PATH=/home/wuc/Mistral-7B-Instruct-v0.2
+#MODEL_PATH=/home/wuc/Mistral-7B-Instruct-v0.2
+MODEL_PATH=/home/wuc/Phi-3-mini-128k-instruct
 
 python3 -m manifest.api.app \
     --model_type huggingface \
     --model_name_or_path ${MODEL_PATH} \
     --model_generation_type text-generation \
-    --fp16 \
-    --device 0
+    --fp16 --device 0 --use_bitsandbytes
+
+# python3 -m manifest.api.app \
+#     --model_type huggingface \
+#     --model_name_or_path ${MODEL_PATH} \
+#     --model_generation_type text-generation \
+#     --fp16 --device 0 --use_accelerate 
