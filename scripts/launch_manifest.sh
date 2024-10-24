d28b9a6ea9229c539d2a996d111294daf67d4e8c
Chen Wu <wu.chen@microsoft.com>
2024-10-24 02:54:18 +0000

    add image model

--- scripts/launch_manifest.sh
+++ scripts/launch_manifest.sh
@@ -1,11 +1,12 @@
 #MODEL_PATH=/home/wuc/Mistral-7B-Instruct-v0.2
-MODEL_PATH=/home/wuc/Phi-3-mini-128k-instruct
+#MODEL_PATH=/home/wuc/Phi-3-mini-128k-instruct
+MODEL_PATH=/home/wuc/Phi-3-vision-128k-instruct
 
 python3 -m manifest.api.app \
     --model_type huggingface \
     --model_name_or_path ${MODEL_PATH} \
     --model_generation_type text-generation \
-    --fp16 --device 0 --use_bitsandbytes
+    --fp16 --device 0
 
 # python3 -m manifest.api.app \
 #     --model_type huggingface \
